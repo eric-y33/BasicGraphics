50c59ddbc44aa69cb38ee26f84ed10621aefb878
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-07-07 16:51:49 -0700

    Add project files.

---
+++ BasicGraphics/Source.cpp
@@ -0,0 +1,88 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// Normalized Device Coordinates for triangle (different from screen coordinates)
+float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f
+};
+
+int main()
+{
+    // Initialize GLFW window settings
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Initialize vertex buffer objects and bind to target
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Copy previously defined vertex data into buffer memory
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Creates window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "TestWindow", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD to manage function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // Size of rendering window (location of lower left corner, dimensions)
+    glViewport(0, 0, 800, 600);
+
+    // Resize viewport if window is resized
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+
+
+    // Render loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // rendering commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // swap buffers and poll IO devices
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+
+    // Clean all allocated resources before quitting
+    glfwTerminate();
+    return 0;
+
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
