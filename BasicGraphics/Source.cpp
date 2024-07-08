f583b4c3a301fe4b2f47ce81a2aa73a9a36d488e
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-07-07 17:23:49 -0700

    added vertex shader

--- BasicGraphics/Source.cpp
+++ BasicGraphics/Source.cpp
@@ -12,21 +12,28 @@ float vertices[] = {
      0.0f,  0.5f, 0.0f
 };
 
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 int main()
 {
+    // Initialize GLAD to manage function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
     // Initialize GLFW window settings
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    // Initialize vertex buffer objects and bind to target
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    // Copy previously defined vertex data into buffer memory
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // Creates window
     GLFWwindow* window = glfwCreateWindow(800, 600, "TestWindow", NULL, NULL);
@@ -38,11 +45,28 @@ int main()
     }
     glfwMakeContextCurrent(window);
 
-    // Initialize GLAD to manage function pointers
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    // Initialize vertex buffer objects and bind to target
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // Copy previously defined vertex data into buffer memory
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Initialize vertex shader object
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    // Attach shader source code to shader object and compile
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Check if vertex shader compiled correctly
+    int  success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
     }
 
     // Size of rendering window (location of lower left corner, dimensions)
@@ -68,8 +92,6 @@ int main()
         glfwPollEvents();
     }
 
-
-
     // Clean all allocated resources before quitting
     glfwTerminate();
     return 0;
@@ -86,3 +108,4 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
