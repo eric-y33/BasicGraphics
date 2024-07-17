3bd83c1df4d4679dd7fb917b38ba4778c06921f5
Eric Y <90221705+eric-y33@users.noreply.github.com>
2024-07-17 15:06:02 -0700

    made half the cubes rotate

--- BasicGraphics/Source.cpp
+++ BasicGraphics/Source.cpp
@@ -274,10 +274,8 @@ int main()
 
         // Projection matrix
         glm::mat4 projection;
-        projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+        projection = glm::perspective(glm::radians(50.0f), 800.0f / 600.0f, 0.5f, 100.0f);
 
-
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 
 
         // Send transformation matrices to shader
@@ -295,6 +293,10 @@ int main()
         {
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
+            if (i % 2 == 0)
+            {
+                model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+            }
             float angle = 20.0f * i;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
